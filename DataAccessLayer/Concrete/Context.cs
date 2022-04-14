baee642f1ebbe4ddfd53a593aa1c12d49a165cda
Süreyya Esin Özdem <esinozdem@gmail.com>
2022-04-15 02:25:18 +0300

    Database'in oluşturulması

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+
+        //bu metodun içerisine connectionstring tanımlayacağız.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-U32R22S\\S2019;database=CoreBlogDb;integrated security=true;");
+        }
+
+        //Tablolarımı tanımlıyorum.
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
