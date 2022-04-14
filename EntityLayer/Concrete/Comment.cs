b0a30058f6357337ab0b7e701e98d0fb11eb6f73
Süreyya Esin Özdem <esinozdem@gmail.com>
2022-04-15 01:43:55 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public string CommentUserName { get; set; } //yorum yapan kişinin adı
+        public string CommentTitle { get; set; }
+        public string CommentContent { get; set; }
+        public DateTime CommentDate { get; set; }
+        public bool CommentStatus { get; set; } 
+    }
+}
